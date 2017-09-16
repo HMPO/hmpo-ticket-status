1682d72d35cdf426a4c4ebf65ca1905cca33a1a4
merlinc <merlinc@thecourtsofchaos.com>
2017-09-16 13:53:07 +0100

    Add tests for JS and JSX
    - Add configuration for Wallaby.js
    - Add configuration for Jest
    - Add working test for JS
    - WIP test for JSX

--- test/unit/lib/spec.api.js
+++ test/unit/lib/spec.api.js
@@ -1,2 +1,33 @@
 'use strict';
 
+const chai = require('chai');
+chai.use(require('chai-string'));
+
+global.chaiExpect = chai.expect;
+
+const Api = require('../../../lib/api');
+
+
+describe('api', () => {
+    describe('getCacheFileName', () => {
+        it('returns cacheFileName for a custom service', () => {
+            let api = new Api({
+                api: 'custom'
+            });
+            let cacheFileName = api.getCacheFileName();
+
+            chaiExpect(cacheFileName).endsWith('cache/custom.json');
+            // expect(cacheFileName).endsWith('cache/custom.json');
+        });
+
+        it('returns a default based upon config.api', () => {
+            let api = new Api();
+            let cacheFileName = api.getCacheFileName();
+
+            chaiExpect(cacheFileName).endsWith('cache/generic.json');
+        });
+    });
+    // it('adds 1 + 2 to equal 3', () => {
+    //     expect(sum(1, 2)).toBe(3);
+    // });
+});
