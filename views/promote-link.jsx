1ca5973208fe506b203c4692714e865e3ddb1747
Paul Winkler <paul.winkler@homeoffice.gov.uk>
2017-11-08 11:53:55 +0000

    Add promotion links for jenkins-promote

---
+++ views/promote-link.jsx
@@ -0,0 +1,16 @@
+'use strict';
+
+const React = require('react');
+const Buildstatus = require('./build-status');
+
+class PromoteLink extends Buildstatus {
+    render() {
+        return (
+            <div className={'promote env ' + this.props.env}>
+                <a href={this.props.link}>Promote to {this.props.env}</a>
+            </div>
+        );
+    }
+}
+
+module.exports = PromoteLink;
