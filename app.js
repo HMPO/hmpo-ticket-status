f22735c1ec5bf6e40fb89b442ea697faa82823d6
Paul Winkler <paul.winkler@digital.homeoffice.gov.uk>
2018-04-19 12:50:57 +0100

    Allow installing as a module and passing config overrides

--- app.js
+++ app.js
@@ -1,34 +1,38 @@
 'use strict';
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const hmpoLogger = require('hmpo-logger');
-const logger = require('./lib/logger');
-const config = require('./lib/config');
+module.exports = function (configData) {
+    const config = require('./lib/config');
+    config.reset(configData);
 
-const routes = {
-    auth: require('./routes/auth'),
-    list: require('./routes/list'),
-    project: require('./routes/project')
+    const express = require('express');
+    const bodyParser = require('body-parser');
+    const hmpoLogger = require('hmpo-logger');
+    const logger = require('./lib/logger');
+
+    const routes = {
+        auth: require('./routes/auth'),
+        list: require('./routes/list'),
+        project: require('./routes/project')
+    };
+
+
+    let app = express();
+
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jsx');
+    app.engine('jsx', require('express-react-views').createEngine());
+
+    app.use(express.static(__dirname + '/public'));
+    app.use(hmpoLogger.middleware());
+
+    app.get('/reset', routes.auth.reset);
+    app.post('/*', bodyParser.urlencoded({extended: true}), routes.auth.login);
+    app.use(routes.auth.checkCredentials);
+    app.get('/', routes.list.get);
+    app.get('/:project', routes.project.get);
+
+    let port = config.get('port');
+    app.listen(port, () => logger.info('Server started on port :port', { port }));
 };
 
-config.reset();
-
-let app = express();
-
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jsx');
-app.engine('jsx', require('express-react-views').createEngine());
-
-app.use(express.static(__dirname + '/public'));
-app.use(hmpoLogger.middleware());
-
-app.get('/reset', routes.auth.reset);
-app.post('/*', bodyParser.urlencoded({extended: true}), routes.auth.login);
-app.use(routes.auth.checkCredentials);
-app.get('/', routes.list.get);
-app.get('/:project', routes.project.get);
-
-let port = config.get('port');
-app.listen(port, () => logger.info('Server started on port :port', { port }));
-
+if (require.main === module) module.exports();
